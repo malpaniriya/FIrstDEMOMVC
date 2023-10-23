26a9c8bfd32f20759dcc3e3474dbbd58f72ac7b8
malpaniriya <riyamalpani75@gmail.com>
2023-10-23 10:43:18 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using FIrstDEMOMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FIrstDEMOMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+
+
+        public IActionResult Index()
+        {
+            ViewData["names"] = "This is the sample text in viewdata";
+            List<string> names = new List<string>()
+            {
+                "Riya","Shruti","Aditya","Vivek","Kishor"
+            };
+            ViewData["list"]= names;
+            return View();
+        }
+
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
